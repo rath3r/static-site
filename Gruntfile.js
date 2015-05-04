198048ea27b404232f76e4fe89fc92def984f500
Thomas Meehan <thomas.meehan@living-group.com>
2015-05-04 21:36:35 +0100

    tidy up

--- Gruntfile.js
+++ Gruntfile.js
@@ -238,7 +238,7 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('assemble');
-	grunt.loadNpmTasks('dp-grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-autoprefixer');
